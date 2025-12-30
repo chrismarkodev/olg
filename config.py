4cf38dbe837b8c71f91848df92f3c738918468c5
cmdev <chrismarkodev@outlook.com>
2025-12-29 23:10:48 -0500

    Initializing project.

---
+++ config.py
@@ -0,0 +1,7 @@
+""" application configuration settings """
+
+# set and retrieve application wide settings
+APP_NAME = 'Lotto649 Data'
+APP_NAME_SHORT = 'L649Data'
+# add year '2024' at the end of the RESULTS_URL string
+RESULTS_URL = "https://ca.lottonumbers.com/lotto-649/numbers/"
